6578b0819ed635d6b5711912d2a0a6f1a9b78efb
HoualefAkram <akram1232004@gmail.com>
2026-03-04 05:46:56 +0100

    add latlng to BS

--- bs.py
+++ bs.py
@@ -1,5 +1,6 @@
 from ue import UE
 from typing import Optional
+from latlng import LatLng
 from ng_ran_report import NGRANReport
 
 
@@ -8,10 +9,12 @@ class BS:
     def __init__(
         self,
         id: int,
+        latlng: LatLng,
         connected_ues: list[UE],
         ng_ran_report: Optional[NGRANReport] = None,
     ):
         self.id = id
+        self.latlng: LatLng = latlng
         self.connected_ues: list[UE] = connected_ues
         self.last_report: NGRANReport = ng_ran_report
 
