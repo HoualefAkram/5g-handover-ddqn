99e79452cdc3561ced18c31a6f3d4f9bfb035c6e
HoualefAkram <akram1232004@gmail.com>
2026-03-23 02:34:49 +0100

    added space and action spaces

--- rl/handover_env.py
+++ rl/handover_env.py
@@ -6,7 +6,9 @@ class HandoverEnv(gym.Env):
 
     def __init__(self):
         super().__init__()
+        # RSRP1, RSRP2, RSRP3, RSRP4, RSRQ1, RSRQ2, RSRQ3, RSRQ4 , Serving BS (OneHotEncoded)
         self.observation_space = Discrete(12)
+        # BS1, BS2, BS3, BS4
         self.action_space = Discrete(4)
 
     def step(self, action):
