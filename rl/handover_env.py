c050aa89a494d037a0765fb51a0d5c1df0025730
HoualefAkram <akram1232004@gmail.com>
2026-03-23 15:15:12 +0100

    init commit for ddqn

--- rl/handover_env.py
+++ rl/handover_env.py
@@ -1,15 +1,20 @@
+import numpy as np
 import gymnasium as gym
-from gymnasium.spaces import Discrete
+from gymnasium.spaces import Box, Discrete
 
 
 class HandoverEnv(gym.Env):
 
     def __init__(self):
         super().__init__()
-        # RSRP1, RSRP2, RSRP3, RSRP4, RSRQ1, RSRQ2, RSRQ3, RSRQ4 , Serving BS (OneHotEncoded)
-        self.observation_space = Discrete(12)
-        # BS1, BS2, BS3, BS4
+
+        # action space: choosing 1 of 4 BS
         self.action_space = Discrete(4)
+
+        # observation Space
+        low = np.array([0] * 8 + [0] * 4)  # Min values for RSRP/RSRQ and One-Hot
+        high = np.array([127] * 8 + [1] * 4)  # Max values
+        self.observation_space = Box(low=low, high=high, dtype=np.int32)
 
     def step(self, action):
         return super().step(action)
