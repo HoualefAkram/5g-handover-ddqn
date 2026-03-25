e9d43dab6534dfebb6228e5196461e7ab556be52
HoualefAkram <akram1232004@gmail.com>
2026-03-25 20:29:15 +0100

    set simulation time to 10mins, update freq to 15ms

--- prepare.py
+++ prepare.py
@@ -11,8 +11,8 @@ MAP_TOP_LEFT = LatLng(51.522004, -0.157535)  # London
 MAP_BOTTOM_RIGHT = LatLng(51.512737, -0.134189)  # London
 MCC = 234  # UK
 OSM_DOWNLOAD_PATH = "cache/maps/map.osm"  # cache folder
-SIMULATION_TIME = 900  # (15 minutes)
-STEP_LENGTH = 0.1  # 10 ms
+SIMULATION_TIME = 600  # (10 minutes)
+STEP_LENGTH = 0.15  # 15 ms
 SEED = 200
 
 
