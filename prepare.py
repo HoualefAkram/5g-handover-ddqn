9b31e529d9678c7042a4366e7772a703e422f3ea
HoualefAkram <akram1232004@gmail.com>
2026-03-27 20:22:58 +0100

    set geo location

--- prepare.py
+++ prepare.py
@@ -7,8 +7,8 @@ from utils.path_gen import PathGeneration
 from utils.tower_downloader import TowerDownloader
 
 # --- Params ---
-MAP_TOP_LEFT = LatLng(55.871303, -4.272429)  # London
-MAP_BOTTOM_RIGHT = LatLng(55.843175, -4.230172)  # London
+MAP_TOP_LEFT = LatLng(51.519411, -0.148076)  # London
+MAP_BOTTOM_RIGHT = LatLng(51.499324, -0.109732)  # London
 MCC = 234  # UK
 OSM_DOWNLOAD_PATH = "cache/maps/map.osm"  # cache folder
 SIMULATION_TIME = 300  # (5 minutes)
